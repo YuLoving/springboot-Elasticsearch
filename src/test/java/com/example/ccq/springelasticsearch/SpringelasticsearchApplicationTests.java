b228905d2bfa5a9da1fa80b40d7cccbd31f6b54d
YuLoving <974456795@qq.com>
2019-07-17 10:15:41 +0800

    添加ES的聚合写法

--- src/test/java/com/example/ccq/springelasticsearch/SpringelasticsearchApplicationTests.java
+++ src/test/java/com/example/ccq/springelasticsearch/SpringelasticsearchApplicationTests.java
@@ -3,6 +3,10 @@ package com.example.ccq.springelasticsearch;
 import com.example.ccq.springelasticsearch.dao.ItemRepository;
 import com.example.ccq.springelasticsearch.pojo.Item;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.aggregations.metrics.avg.InternalAvg;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
@@ -13,6 +17,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.query.FetchSourceFilter;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -255,6 +261,103 @@ public class SpringelasticsearchApplicationTests {
         }
     }
 
+    /**
+     * 注意：在ES中，需要进行聚合、排序、过滤的字段其处理方式比较特殊，因此不能被分词。
+     * 这里我们将color和make这两个文字类型的字段设置为keyword类型，这个类型不会被分词，将来就可以参与聚合
+     *
+     */
 
+    //桶就是分组，比如这里我们按照品牌brand进行分组：
+
+            /**
+             * AggregationBuilders：聚合的构建工厂类。所有聚合都由这个类来构建
+                     *  （1）统计某个字段的数量
+                     *   ValueCountBuilder vcb=  AggregationBuilders.count("count_uid").field("uid");
+                     * （2）去重统计某个字段的数量（有少量误差）
+                     *  CardinalityBuilder cb= AggregationBuilders.cardinality("distinct_count_uid").field("uid");
+                     * （3）聚合过滤
+                     * FilterAggregationBuilder fab= AggregationBuilders.filter("uid_filter").filter(QueryBuilders.queryStringQuery("uid:001"));
+                     * （4）按某个字段分组
+                     * TermsBuilder tb=  AggregationBuilders.terms("group_name").field("name");
+                     * （5）求和
+                     * SumBuilder  sumBuilder=	AggregationBuilders.sum("sum_price").field("price");
+                     * （6）求平均
+                     * AvgBuilder ab= AggregationBuilders.avg("avg_price").field("price");
+                     * （7）求最大值
+                     * MaxBuilder mb= AggregationBuilders.max("max_price").field("price");
+                     * （8）求最小值
+                     * MinBuilder min=	AggregationBuilders.min("min_price").field("price");
+                     * （9）按日期间隔分组
+                     * DateHistogramBuilder dhb= AggregationBuilders.dateHistogram("dh").field("date");
+                     * （10）获取聚合里面的结果
+                     * TopHitsBuilder thb=  AggregationBuilders.topHits("top_result");
+                     * （11）嵌套的聚合
+                     * NestedBuilder nb= AggregationBuilders.nested("negsted_path").path("quests");
+                     * （12）反转嵌套
+                     * AggregationBuilders.reverseNested("res_negsted").path("kps ");
+             *
+             *  AggregatedPage：聚合查询的结果类。它是Page<T>的子接口：
+             *  AggregatedPage在Page功能的基础上，拓展了与聚合相关的功能，它其实就是对聚合结果的一种封装。
+             */
+    @Test
+    public void testagg(){
+        //构建查询条件
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+        //不查询任何结果
+         builder.withSourceFilter(new FetchSourceFilter(new String[]{""},null));
+        // 1、添加一个新的聚合，聚合类型为terms，聚合名称为brands，聚合字段为brand
+        builder.addAggregation(AggregationBuilders.terms("brands").field("brand"));
+        //2.查询，需要把结果强转为AggregatedPage类型
+       AggregatedPage<Item> aggpage= (AggregatedPage<Item>) itemRepository.search(builder.build());
+        // 3、解析
+        // 3.1、从结果中取出名为brands的那个聚合，
+        // 因为是利用String类型字段来进行的term聚合，所以结果要强转为StringTerm类型
+       StringTerms agg= (StringTerms) aggpage.getAggregation("brands");
+        // 3.2、获取桶
+        List<StringTerms.Bucket> buckets = agg.getBuckets();
+        // 3.3、遍历
+        for (StringTerms.Bucket bucket:buckets ) {
+            // 3.4、获取桶中的key，即品牌名称
+            System.out.println(bucket.getKeyAsString());
+            //获取桶中的文档数量
+            System.out.println(bucket.getDocCount());
+        }
+
+
+    }
+
+
+    /**
+     * 嵌套聚合，求平均值
+     */
+
+    @Test
+    public void testsubagg(){
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+        //不查询然后结果
+        builder.withSourceFilter(new FetchSourceFilter(new String[]{""},null));
+        //添加一个新的聚会，聚合类型为terms,聚合名称为brands,聚合字段为brand
+        builder.addAggregation(
+                AggregationBuilders.terms("brands").field("brand")
+                .subAggregation(AggregationBuilders.avg("priceavg").field("price")) // 在品牌聚合桶内进行嵌套聚合，求平均值
+        );
+        //查询，把结果强转为AggregatedPage类型
+        AggregatedPage<Item> aggregatedPage = (AggregatedPage<Item>) itemRepository.search(builder.build());
+        //解析
+        //从结果中取出名为brands的那个聚合，
+        //因为是利用String类型字段来进行的term聚合，所以结果要强转为StringTerm类型
+        StringTerms agg = (StringTerms) aggregatedPage.getAggregation("brands");
+        //获取桶
+        List<StringTerms.Bucket> buckets = agg.getBuckets();
+        //遍历
+        for (StringTerms.Bucket bucket:buckets) {
+            //获取桶中的key，即品牌名称.获取桶中的文档数量
+            System.out.println("品牌："+bucket.getKeyAsString()+"数量："+bucket.getDocCount());
+            //获取子聚合结果：
+           InternalAvg avg = (InternalAvg) bucket.getAggregations().asMap().get("priceavg");
+            System.out.println("平均售价："+avg.getValue());
+        }
+
+    }
 
 }
