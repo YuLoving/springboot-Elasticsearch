36519095c6c6ada3cf03342005a5f8aaf5b6f0b6
YuLoving <974456795@qq.com>
2019-07-18 15:48:24 +0800

    添加简单的ajax请求登录

---
+++ src/main/java/com/example/ccq/springelasticsearch/controller/TestController.java
@@ -0,0 +1,49 @@
+package com.example.ccq.springelasticsearch.controller;
+
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/index")
+    public String toindex(){
+        return "index";
+    }
+
+
+    @GetMapping("/login")
+    public String tologin(){
+        return "login";
+    }
+    /**
+     * 测试登录
+     */
+    @PostMapping("/tologin")
+    @ResponseBody
+    public Object login(String name, String pwd){
+        Map<String, Object> map = new HashMap<>();
+        System.out.println("name:"+name+",pwd:"+pwd);
+        if(name.equals("aaa") && pwd.equals("123")){
+            //登录失败
+            map.put("code",200);
+            map.put("msg","/test/index");
+        }else{
+            //登录成功
+            map.put("code",500);
+            map.put("msg","/test/login");
+        }
+        return map;
+
+    }
+}
