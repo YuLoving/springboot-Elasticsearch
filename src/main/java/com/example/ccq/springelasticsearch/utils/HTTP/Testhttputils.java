2922690e07ff22ac185ec7a08dc0869433ba4772
YuLoving <974456795@qq.com>
2019-08-19 15:56:59 +0800

    新增H

--- src/main/java/com/example/ccq/springelasticsearch/utils/HTTP/Testhttputils.java
+++ src/main/java/com/example/ccq/springelasticsearch/utils/HTTP/Testhttputils.java
@@ -1,25 +1,37 @@
 package com.example.ccq.springelasticsearch.utils.HTTP;
 
-import com.alibaba.fastjson.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Testhttputils {
     public static void main(String[] args) throws Exception {
-       /* //get请求
-        String url="http://localhost:8186/get";
+        //get请求
+       /* String url="http://localhost:8186/get";
         Map<String, String> map = new HashMap<>();
         map.put("a","1");
         map.put("b","2");
 
 
         String s = HttpClientUtil.doGet(url,map);
-        System.out.println(s);
-*/
-        String url="http://localhost:8186/postjson";
+        System.out.println(s);*/
+
+
+       /* String url="http://localhost:8186/postjson";
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("唉唉","1");
         jsonObject.put("aa","2");
         System.err.println(jsonObject.toString());
         String s = HttpClientUtil.doPostJson(url,jsonObject.toString());
+        System.out.println(s);*/
+
+
+        String url="http://localhost:8186/postaa";
+        Map<String, String> map = new HashMap<>();
+        map.put("a","1");
+        map.put("b","2");
+        System.err.println(map.toString());
+
+        String s = HttpClientUtil.doPost(url,map);
         System.out.println(s);
     }
 }
--- src/test/java/com/example/ccq/springelasticsearch/thread/Test.java
+++ src/test/java/com/example/ccq/springelasticsearch/thread/Test.java
@@ -1,7 +1,12 @@
 package com.example.ccq.springelasticsearch.thread;
 
+import com.example.ccq.springelasticsearch.utils.IdcardValidator;
+
 public class Test {
     public static void main(String[] args) {
+        String idcard="320681199409051417";
 
+
+        System.out.println(new IdcardValidator(idcard.toUpperCase()).validate());
     }
 }
